615ec57cc117b62a8df723e97709fac41ca88022
randomguycuzimbored <lian.zhi@ufl.edu>
2026-09-25 19:48:55 -0400

    Dwqdw

--- bitops.cpp
+++ bitops.cpp
@@ -15,3 +15,15 @@ unsigned int clearBit(unsigned int x, int n){
 unsigned int toggleBit(unsigned int x, int n){
     return x ^ (1u << n);
 }
+
+int countOnes(unsigned int x){
+    int count=0;
+
+    while(x!=0){
+        count += x & 1u;
+        x >>=1;
+    }
+
+    return count;
+}
+
